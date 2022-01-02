d543960302cfa0442f2b2965c2541190e8405d04
Tasheem Hargrove <tasheem@Tasheems-MBP.attlocal.net>
2022-01-02 17:55:40 -0600

    Construct description for SEND transactions in TransactionService's create()

--- src/main/java/com/hargrove/services/TransactionService.java
+++ src/main/java/com/hargrove/services/TransactionService.java
@@ -4,6 +4,8 @@ import com.hargrove.dao.TransactionDAO;
 import com.hargrove.enums.TransactionCategory;
 import com.hargrove.models.CheckingAccount;
 import com.hargrove.models.Transaction;
+import com.hargrove.models.User;
+
 import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
@@ -11,6 +13,7 @@ import java.util.UUID;
 public class TransactionService {
     TransactionDAO dao;
     CheckingAccountService checkingService;
+    UserService userService;
 
     public List<Transaction> get(String userID) {
         checkingService = new CheckingAccountService();
@@ -35,7 +38,13 @@ public class TransactionService {
         transaction.setDate(date);
 
         if(transaction.getCategory() == TransactionCategory.SEND) {
-            String description = "$" + transaction.getAmount() + " sent";
+            userService = new UserService();
+            CheckingAccount receiverAccount = checkingService.getAccount(transaction.getDestinationAccountNumber());
+            User receiver = userService.getUser(receiverAccount.getUserID());
+
+            String description = String.format("$%s sent to %s %s", transaction.getAmount().toString(),
+                    receiver.getFirstName(), receiver.getLastName());
+            transaction.setDescription(description);
         }
         else if(transaction.getCategory() == TransactionCategory.WITHDRAWAL) {
             String description = "Withdrew $" + transaction.getAmount();
